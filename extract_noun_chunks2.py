ccae3f81bf7ff1d8608558a2b627fddab98c36dd
Rupinder <rupindermonga@gmail.com>
2020-07-10 03:48:27 +0530

    included ranking with overall freq

--- extract_noun_chunks2.py
+++ extract_noun_chunks2.py
@@ -5,6 +5,7 @@ import json
 import os
 import csv
 import time
+import pandas as pd
 
 start_time = time.time()
 nlp = spacy.load('en_core_web_sm')
@@ -32,7 +33,7 @@ for filename in json_files:
                         # count += 1
                         # if count == 5:
                         #     break   
-                        text = r.get('reviewText','').strip()
+                        text = r.get('reviewText','').lower().strip() #lowered all chunks
                         if text != '':
                             texts.append(text)
                 else:
@@ -67,8 +68,8 @@ for filename in json_files:
 
 
 # out = csv.writer(open('/media/rupinder/C49A5A1B9A5A0A76/Users/Rupinder/Desktop/BVR/Warning_code/feature_analysis-master/electric-kettles/ek.csv'.format(in_slug),'w'))
-out = csv.writer(open(os.path.join(path_to_json, "ek1.csv"),'w'))
-out2 = csv.writer(open(os.path.join(path_to_json, "ek2.csv"),'w'))
+out = csv.writer(open(os.path.join(path_to_json, "ek.csv"),'w'))
+# out2 = csv.writer(open(os.path.join(path_to_json, "ek2.csv"),'w'))
 
 
 # out = csv.writer(open('/media/rupinder/C49A5A1B9A5A0A76/Users/Rupinder/Desktop/BVR/feature_analysis-master/electric-kettles/ek1.csv','w'))
@@ -83,17 +84,29 @@ for k, v in final_dict.items():
     for s in sort:
         out.writerow([s,k,v[s]])
 
-for k, v in final_dict.items():
-    sort = sorted(v, key=lambda k1:v[k1], reverse=True)
-    for s in sort:
-        if v[s] > 100:
-            out2.writerow([s,k,v[s]])
 
+# for k, v in final_dict.items():
+#     sort = sorted(v, key=lambda k1:v[k1], reverse=True)
+#     for s in sort:
+#         if v[s] > 100:
+#             out2.writerow([s,k,v[s]])
 
+updated_data = pd.read_csv(os.path.join(path_to_json, "ek.csv"))
 # sort = sorted(freq, key=lambda k:freq[k], reverse=True)
 # for s in sort:
 #     out.writerow([s,freq[s]])
 
-    
+print(type(updated_data))
+# updated_data['']
+
+updated_data.columns = ['noun_chunk', 'product', 'freq']
+
+updated_data['overall_freq'] = updated_data.groupby('noun_chunk').freq.transform('sum')
+
+updated_data["Rank"] = updated_data["overall_freq"].rank( ascending = False, method = "dense")
+
+updated_data.sort_values("Rank", inplace = True)
+
+updated_data.to_csv(os.path.join(path_to_json, "cg_final.csv"))
 print(time.time()-start_time)
     
